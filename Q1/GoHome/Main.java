053c421f3dcda71deb7b21f2a4f516c4915e8d13
FelixJaschul <129108886+FelixJaschul@users.noreply.github.com>
2025-01-14 07:04:58 +0100

    GoHome

---
+++ Q1/GoHome/Main.java
@@ -0,0 +1,11 @@
+// Felix, Darek, Titus
+// Spiel "Go Home" programmieren
+// Klasse "Main" programmieren
+
+// ausführen des Spiels
+public class Main {
+    public static void main(String[] args) {
+        Spiel spiel = new Spiel();
+        spiel.mainSpiel();
+    }
+}
