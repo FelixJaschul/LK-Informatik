c8c30bcba2950734a401d9ea7c21e34124f88430
FelixJaschul <129108886+FelixJaschul@users.noreply.github.com>
2025-01-14 13:30:50 +0000

    14.01.2024

--- Q2/Beziehungen zwischen Klasse/Aufgabe-1/Buch.java
+++ Q2/Beziehungen zwischen Klasse/Aufgabe-1/Buch.java
@@ -2,8 +2,17 @@ public class Buch {
     String titel;
     String datum;
 
-    public buchDetails(String titel, String datum, Autor name, Autor alter) {
+    public static void main(String[] args) {
+        buchDetails("Dune", "14.01.2004", "Felix Jaschul", 57);
+    }
+
+    public Buch(String titel, String datum) {
+        this.titel = titel;
+        this.datum = datum;
+    }
+
+    public void buchDetails(String titel, String datum, Autor name, Autor alter) {
         System.out.println("Das Buch: " + titel + " wurde " + datum + " veröffentlicht.");
-        System.out.println("Der Autor " + this.name + " ist " + this.alter + " Jahre alt.");
+        System.out.println("Der Autor " + name + " ist " + alter + " Jahre alt.");
     }
 }
