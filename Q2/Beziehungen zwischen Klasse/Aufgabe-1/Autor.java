bb69a2a94fcc0fdba466ca1c2a999fab0a2ed1b6
FelixJaschul <129108886+FelixJaschul@users.noreply.github.com>
2025-01-16 07:58:15 +0000

    16.01.2024

--- Q2/Beziehungen zwischen Klasse/Aufgabe-1/Autor.java
+++ Q2/Beziehungen zwischen Klasse/Aufgabe-1/Autor.java
@@ -1,9 +1,15 @@
 public class Autor {
     String name;
-    String alter;
+    int alter;
 
-    public Autor(String name, String alter) {
+    // Constructor
+    public Autor(String name, int alter) {
         this.name = name;
-        this.alter = alter;    
-    } 
+        this.alter = alter;
+    }
+
+    // Non-static method for displaying author details
+    public void autorDetails() {
+        System.out.println("Der Autor " + name + " ist " + alter + " Jahre alt.");
+    }
 }
--- Q2/Beziehungen zwischen Klasse/Aufgabe-1/Buch.java
+++ Q2/Beziehungen zwischen Klasse/Aufgabe-1/Buch.java
@@ -2,17 +2,24 @@ public class Buch {
     String titel;
     String datum;
 
-    public static void main(String[] args) {
-        buchDetails("Dune", "14.01.2004", "Felix Jaschul", 57);
-    }
-
+    // Konstruktor
     public Buch(String titel, String datum) {
         this.titel = titel;
         this.datum = datum;
     }
 
-    public void buchDetails(String titel, String datum, Autor name, Autor alter) {
-        System.out.println("Das Buch: " + titel + " wurde " + datum + " veröffentlicht.");
-        System.out.println("Der Autor " + name + " ist " + alter + " Jahre alt.");
+    // Non-static method um Buch Details darzustellen
+    public void buchDetails() {
+        System.out.println("Das Buch: " + titel + " wurde am " + datum + " veröffentlicht.");
+    }
+
+    public static void main(String[] args) {
+        // Boch Obejekt erstellen
+        Buch buch = new Buch("Dune", "14.01.2004");
+        buch.buchDetails();
+
+        // Autor Objekt erstellen
+        Autor autor = new Autor("Frank Herbert", 65);
+        autor.autorDetails();
     }
 }
